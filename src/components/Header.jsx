8f149f2ea4b36881a9dd1b078edec704d9e8f300
Anadi Vaidya <anadivaidya33@gmail.com>
2026-07-11 15:25:26 +0530

    Fixed a typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({score, highScore}) {
       <div className="header">
         <div className="header-title">
           <h1>The Memory Card Game</h1>
-          <label>Remeber your Pokémon</label>
+          <label>Remember your Pokémon</label>
         </div>
         <div className="header-details">
           <h2>Score: {score}</h2>

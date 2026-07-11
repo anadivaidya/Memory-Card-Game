a3fee23229cb7a2fc4e7d42388ef27a7fefd1b52
Anadi Vaidya <anadivaidya33@gmail.com>
2026-07-11 15:13:35 +0530

    Title and Favicon Change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header({score, highScore}) {
       <div className="header">
         <div className="header-title">
           <h1>The Memory Card Game</h1>
+          <label>Remeber your Pokémon</label>
         </div>
         <div className="header-details">
           <h2>Score: {score}</h2>
